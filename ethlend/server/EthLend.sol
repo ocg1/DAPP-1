d823a5aaaae43e8bbfd9d4e3bfa52da1be532701
Jitendra Chittoda <jcchittoda@gmail.com>
2017-10-11 05:40:20 +0530

    AbstractENS events are not required for interface

--- ethlend/server/EthLend.sol
+++ ethlend/server/EthLend.sol
@@ -49,18 +49,6 @@ contract AbstractENS {
      function setSubnodeOwner(bytes32 node, bytes32 label, address owner);
      function setResolver(bytes32 node, address resolver);
      function setTTL(bytes32 node, uint64 ttl);
-
-     // Logged when the owner of a node assigns a new owner to a subnode.
-     event NewOwner(bytes32 indexed node, bytes32 indexed label, address owner);
-
-     // Logged when the owner of a node transfers ownership to a new account.
-     event Transfer(bytes32 indexed node, address owner);
-
-     // Logged when the resolver for a node changes.
-     event NewResolver(bytes32 indexed node, address resolver);
-
-     // Logged when the TTL of a node changes
-     event NewTTL(bytes32 indexed node, uint64 ttl);
 }
 
 contract Registrar {
